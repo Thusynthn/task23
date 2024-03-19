9a80bd9b25553376fdf712e39e7b765ee016527a
Thusyanthan Ravichandran <thusyanthan.r@outlook.com>
2024-03-19 23:48:05 +0000

    more effiecient code

--- manipulation.py
+++ manipulation.py
@@ -1,4 +1,3 @@
-
 # Prompt the user to enter a sentence
 str_manip = input("Enter your sentence:\n")
 
@@ -6,21 +5,15 @@ str_manip = input("Enter your sentence:\n")
 length_str_manip = len(str_manip)
 print("Length of the given sentence: ", length_str_manip)
 
-# Find the last letter of the input sentence
-last_letter = str_manip[-1]
-
 # Replace the last letter of the input sentence with '@' and print the modified sentence
-str_manip_replaced = str_manip.replace(last_letter, '@')
+str_manip_replaced = str_manip[:-1] + '@'
 print("String with replaced last letter:\n", str_manip_replaced)
 
-# Extract the last three characters of the input sentence in reverse order
+# Print the last three characters of the input sentence in reverse order
 last_three_reverse = str_manip[-1:-4:-1]
-
-# Print the last three characters in reverse order
 print("Last three characters in reverse:\n", last_three_reverse)
 
 # Form a new word by concatenating the first three characters and the last two characters of the input sentence
 five_letter_word = str_manip[:3] + str_manip[-2:]
-
-# Print the newly formed five-letter word
 print("Five letter word:\n", five_letter_word)
+
